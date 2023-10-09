bde213bb83f9332c781d14e6da242c6511eb3e17
jonathanholmgren <jonathan.holmgren@hotmail.com>
2023-10-09 03:06:52 +0200

    New ID

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,20 +1,24 @@
 package org.main;
 
 import org.entities.Product;
+import org.service.Warehouse;
 
 import java.util.List;
 
 import static org.mockdata.MockData.getProducts;
+import static org.service.Warehouse.createANewProduct;
+
 
 public class Main {
     public static void main(String[] args) {
 
-            List<Product> productsList = getProducts();
+      List<Product>  productsList = Warehouse.getProductList();
 
             for (Product product: productsList) {
                 System.out.print(product.getName() + ": ");
                 System.out.println(product.getCategory());
             }
-
+        createANewProduct();
     }
+
 }
--- src/main/java/org/service/Warehouse.java
+++ src/main/java/org/service/Warehouse.java
@@ -2,23 +2,36 @@ package org.service;
 
 import org.entities.Product;
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.mockdata.MockData.getProducts;
 
 public class Warehouse {
 
+    static List<Product> productList = getProducts();
 
 
-    public static void createANewProduct(String name, double rating, double price, Product.Category category, Product.SubCategory subCategory){
+    public static List<Product> getProductList() {
+        return productList;
+    }
 
+    public static void createANewProduct(){
+
+        //String name, double rating, double price, Product.Category category, Product.SubCategory subCategory
         Date createdDate = new Date();
         Date lastmodified = new Date();
-        String idd;
 
+        int newId = productList.stream()
+                .mapToInt(Product::getId)
+                .max()
+                .orElse(0) + 1;
 
-        Product newProduct = new Product(1,"BASIC HEAVY WEIGHT T-SHIRT",3,17.99,Product.Category.MAN,Product.SubCategory.TSHIRTS,createdDate,lastmodified);
+        System.out.println(newId);
+
+       Product newProduct = new Product(newId,"BASIC HEAVY WEIGHT T-SHIRT",3,17.99,Product.Category.MAN,Product.SubCategory.TSHIRTS,createdDate,lastmodified);
 
         //Gör kod så den kollar högst Id i listan och sen plusar på ett och sparar ner det som nytt id.
         // Set till att name fältet inte får vara tomt
